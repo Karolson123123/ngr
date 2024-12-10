84d82aeef226853ddf77b542be53f853f163b5bf
Karolson123123 <karoldrozdowski1221@gmail.com>
2024-12-10 21:56:27 +0100

    sdas

---
+++ script.js
@@ -0,0 +1,26 @@
+let countDownDate = new Date("Mar 2, 2025 00:00:00").getTime();
+
+
+let x = setInterval(function() {
+
+  
+  let now = new Date().getTime();
+
+  
+  let distance = countDownDate - now;
+
+  
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  
+  document.getElementById("countdown").innerHTML = `<div><div>${days} dni</div><div> ${hours} ${}</div><div> ${minutes}m</div><div> ${seconds}s</div></div>`;
+
+  
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000)
