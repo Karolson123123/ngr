9510a47616068ecc1c2f813933f4c0cf8dd0bfc3
kicporr <kacper.teichert6790@gmail.com>
2024-12-11 17:47:17 +0100

    karty o nas

---
+++ js/aboutus.js
@@ -0,0 +1,11 @@
+// Obrót kartą po kliknięciu
+
+document.addEventListener("DOMContentLoaded", () => {
+    const cards = document.querySelectorAll('.card');
+
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+            card.classList.toggle('flipped');
+        });
+    });
+});
